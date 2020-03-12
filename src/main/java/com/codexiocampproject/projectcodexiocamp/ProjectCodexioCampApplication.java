ca3ecd2fe6514f264475fb12cee7c25f6af486e2
lucho <https://github.com/L-Ivanov/>
2020-03-12 16:22:07 +0200

    Message

---
+++ src/main/java/com/codexiocampproject/projectcodexiocamp/ProjectCodexioCampApplication.java
@@ -0,0 +1,13 @@
+package com.codexiocampproject.projectcodexiocamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodexioCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCodexioCampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codexiocampproject/projectcodexiocamp/ProjectCodexioCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexiocampproject.projectcodexiocamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodexioCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
